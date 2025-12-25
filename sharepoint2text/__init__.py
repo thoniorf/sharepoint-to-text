a2677cdc561afdb6df42b520905354cdd306dfe8
Tobias Horsamnn <tobias.horsmann@gmail.com>
2025-12-25 16:33:12 +0100

    increased dev version

--- sharepoint2text/__init__.py
+++ sharepoint2text/__init__.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 from sharepoint2text.router import get_extractor, is_supported_file
 
-__version__ = "0.1.1.dev31"
+__version__ = "0.1.1.dev32"
 
 
 def read_docx(file_like: io.BytesIO):
